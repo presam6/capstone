16ef9254da2cabae08c215d2b8bc7ebc035307e2
Mcquinley Josh Maglangit <maglangit.mjosh@gmail.com>
2024-09-21 09:05:21 +0800

    edit calendar page

--- front-end/pages/Calendar/Calendar.jsx
+++ front-end/pages/Calendar/Calendar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Calendar as BigCalendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import enUS from 'date-fns/locale/en-US';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { BookingContext } from './BookingContext';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+  const { bookings } = useContext(BookingContext);
 
-export default Calendar
+  // Map bookings to events format required by react-big-calendar
+  const events = bookings.map((booking, index) => ({
+    title: `Room ${booking.roomNumber} Booking`,
+    start: new Date(booking.checkin),
+    end: new Date(booking.checkout),
+    allDay: true,
+  }));
+
+  return (
+    <div>
+      <h2>Booking Calendar</h2>
+      <BigCalendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
